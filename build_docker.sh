b3c01ada64f9eb1890de6556869e4fc5f4dd0998
Alan Li <61896187+lebr0nli@users.noreply.github.com>
2024-03-05 19:08:31 +0800

    update build script (#2)

--- build_docker.sh
+++ build_docker.sh
@@ -1,11 +1,3 @@
-# build the docker image and run the container
+#!/bin/bash
+
 docker-compose up -d
-
-# follow the logs
-docker-compose logs -f
-
-# stop the container but keep the container
-docker-compose stop
-
-# stop the container and discard the container
-docker-compose down
